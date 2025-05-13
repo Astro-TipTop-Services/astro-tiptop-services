bccf37e5eefbc9b83c2ac824dd0311cd095ad562
lmazzolo <lisa-marie.mazzolo@lam.fr>
2025-05-13 14:04:36 +0200

    configure GitHub Pages for the project

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,15 +15,15 @@ const config = {
   favicon: 'img/logo_astro-tiptop.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://lmazzolo.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/astro-tiptop-services/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'astro-tiptop', // Usually your GitHub org/user name.
+  projectName: 'astro-tiptop-services', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',

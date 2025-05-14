a15265b5fa50474a066f0b9b4a32df892b66071c
lmazzolo <lisa-marie.mazzolo@lam.fr>
2025-05-14 13:51:11 +0200

    definition of a React component in the astro_tiptop_modules.js page

---
+++ src/pages/astro_tiptop_modules.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function AstroTipTopModules() {
+  return (
+    <Layout title="Astro-TipTop Services">
+      <main className="container margin-vert--xl">
+        <h1>Astro-TipTop Services</h1>
+        <p>Coming soon...</p>
+      </main>
+    </Layout>
+  );
+}
